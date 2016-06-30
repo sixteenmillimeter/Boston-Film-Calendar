e6ccd8cb972b083f2b0cd25870d01d70321ed20f
Matt McWilliams <matt@downplaneconsulting.com>
2016-06-30 13:28:30 -0400

    Procfile and testbed restify setup

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-var app = {};
+var restify = require('restify'),
+	port = process.env.PORT || 8080;
+
+var server = restify.createServer({
+  name: 'bostonfilm',
+  version: '1.0.0'
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/', function (req, res, next) {
+  res.end('hi matt');
+  return next();
+});
+
+server.listen(port, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
