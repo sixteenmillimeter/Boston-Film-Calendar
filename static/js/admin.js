a13931772ea9939b3aa9369c37f83be41f38d53e
Matt <sixteenmillimeter@riseup.net>
2016-08-13 19:07:09 -0400

    Get month data for admin console

--- static/js/admin.js
+++ static/js/admin.js
@@ -14,6 +14,40 @@ var months = [
 	'december'
 ];
 
+var position;
+
 $(document).ready(function () {
-	
+	position = moment();
+	getMonth();
 });
+
+var next = function () {
+	'use strict';
+	position.add(1, 'months');
+	getMonth();
+};
+
+var prev = function () {
+	'use strict';
+	position.subtract(1, 'months');
+	getMonth();
+};
+
+var getMonth = function () {
+	'use strict';
+	var month = position.format('MM'),
+		year = position.format('YYYY'),
+		obj = {
+			url : '/calendar/' + month + '/' + year,
+			method : 'GET',
+			dataType : 'json',
+			success : function (data) {
+				console.log(data);
+			},
+			error : function (err) {
+				console.error(err);
+			}
+ 		};
+	$.ajax(obj);
+};
+
