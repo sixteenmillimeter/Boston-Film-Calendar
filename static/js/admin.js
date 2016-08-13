251bc84f7e800a55e077e55fb82ea047d012cb42
Matt <sixteenmillimeter@riseup.net>
2016-08-13 18:59:20 -0400

    Add moment.js and admin.js

---
+++ static/js/admin.js
@@ -0,0 +1,19 @@
+var months = [
+	'blank',
+	'january',
+	'february',
+	'march',
+	'april',
+	'may',
+	'june',
+	'july',
+	'august',
+	'september',
+	'october',
+	'november',
+	'december'
+];
+
+$(document).ready(function () {
+	
+});
